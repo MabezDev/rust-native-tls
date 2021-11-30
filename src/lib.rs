87b75052ce452ad2254f25e0adfd7c3680e3dee8
Scott Mabin <scott@mabez.dev>
2021-11-30 15:58:34 +0000

    Initial mbed impl, tls acceptor refactored

--- src/lib.rs
+++ src/lib.rs
@@ -116,6 +116,9 @@ mod imp;
 #[cfg(target_os = "windows")]
 #[path = "imp/schannel.rs"]
 mod imp;
+// #[cfg(target_os = "espidf")]
+#[path = "imp/mbedtls.rs"]
+mod imp;
 #[cfg(not(any(target_os = "macos", target_os = "windows", target_os = "ios")))]
 #[path = "imp/openssl.rs"]
 mod imp;
